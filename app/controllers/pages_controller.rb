b25976b99d170c28f366d5b206d35b446acc7172
j-keravica <jelena.keravica@gmail.com>
2015-07-19 11:16:30 +0200

    Added text extraction from a web page (p and h1) and splitting

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,23 @@
+require 'open-uri'
+
 class PagesController < ApplicationController
 
 	def new
 	end
 
 	def create
-		title = Nokogiri::HTML::Document.parse(HTTParty.get(params[:page][:url]).body).title
+		#title = Nokogiri::HTML::Document.parse(HTTParty.get(params[:page][:url]).body).title
 		#render plain: params[:page].inspect
+		url = params[:page][:url]
+		doc = Nokogiri::HTML(open(url))
+		text = ""
+		doc.xpath('//h1 | //p').each do |node|
+	      	text = text + " " + node.text
+	    end
+	    #puts text.split
+	    title = doc.xpath('//title').text
 		@user = current_user
-		@page = Page.new(:url => params[:page][:url], :title => title, :text => "bla", :user_id => @user.id)
+		@page = Page.new(:url => url, :title => title, :text => text, :user_id => @user.id)
 		@page.save
 		redirect_to @page
 	end
