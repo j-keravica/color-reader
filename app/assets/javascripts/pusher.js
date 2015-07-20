f08aee6e8439d360498855bf831216ad7e2424d5
j-keravica <jelena.keravica@gmail.com>
2015-07-20 20:15:51 +0200

    Color information is now being correctly displayed

--- app/assets/javascripts/pusher.js
+++ app/assets/javascripts/pusher.js
@@ -9,6 +9,7 @@ var pusher = new Pusher('99b6cb58a1159ad76403', {
 		var ctx = c.getContext("2d");
 		ctx.clearRect(0, 0, c.width, c.height);
 		ctx.font = "30px Arial";
+		ctx.fillStyle = data.color;
 		ctx.fillText(data.word,10,50);
 		//alert(data.word);
     });
