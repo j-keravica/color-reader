74983ce77edd704a151ee403701b814a139f02c7
j-keravica <jelena.keravica@gmail.com>
2015-07-20 19:43:36 +0200

    Incoming data displayed in canvas
    Each new word arriving through pusher is now being displayed in a canvas html element

--- app/assets/javascripts/pusher.js
+++ app/assets/javascripts/pusher.js
@@ -1,7 +1,14 @@
 var pusher = new Pusher('99b6cb58a1159ad76403', {
-      encrypted: true
+      encrypted: true,
+      disableStats: true
     });
     var channel = pusher.subscribe('test_channel');
     channel.bind('my_event', function(data) {
-      alert(data.word);
+
+    	var c = document.getElementById("myCanvas");
+		var ctx = c.getContext("2d");
+		ctx.clearRect(0, 0, c.width, c.height);
+		ctx.font = "30px Arial";
+		ctx.fillText(data.word,10,50);
+		//alert(data.word);
     });
