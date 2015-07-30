c015738693aafbec51104bb2467f8a4edd59c9dd
j-keravica <jelena.keravica@gmail.com>
2015-07-30 14:13:45 +0200

    Some more examples

--- spec/lib/word_scheduler_spec.rb
+++ spec/lib/word_scheduler_spec.rb
@@ -2,19 +2,33 @@ require "rails_helper"
 
 describe "Word Scheduler" do
 
-  before :each do
-    @scheduler = WordScheduler.new(60, ["These", "are", "some", "nice", "words"], "b")
+  describe "#interval" do
+
+    it "calculates the time interval for sending words" do
+      examples = [
+        { :input => 60, :result => "1.0s" },
+        { :input => 100, :result => "0.6s" },
+        { :input => 200, :result => "0.3s" }
+      ]
+      examples.each do |example|
+        input = example[:input]
+        @scheduler = WordScheduler.new(input, [], "b")
+        expect(@scheduler.interval).to eq(example[:result])
+      end
+    end
+
   end
 
-  it "calculates the time interval for sending words" do
-    expect(@scheduler.interval).to eq("1.0s")
-  end
 
+  describe "#send" do
 
-  it "sends the words with the color information" do
-    ENV["COLOR_URL"] = "test"
-    expect(RestClient).to receive(:post).with(ENV["COLOR_URL"] + '/color', {:word => "nesto", :color => "b"})
-    @scheduler.send("nesto")
+    it "sends the words with the color information" do
+      @scheduler = WordScheduler.new(60, [], "b")
+      ENV["COLOR_URL"] = "test"
+      expect(RestClient).to receive(:post).with(ENV["COLOR_URL"] + '/color', {:word => "word", :color => "b"})
+      @scheduler.send("word")
+    end
+
   end
 
 end
