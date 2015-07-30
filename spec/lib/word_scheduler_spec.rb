6932cb0f9fe8a80ade31f54606e5a7a773284caf
j-keravica <jelena.keravica@gmail.com>
2015-07-30 22:55:12 +0200

    Added job_id check for scheduler

--- spec/lib/word_scheduler_spec.rb
+++ spec/lib/word_scheduler_spec.rb
@@ -22,11 +22,19 @@ describe "Word Scheduler" do
   describe "#start_sending" do
 
     it "periodically sends the words with the color information" do
+      pending "rethinking my design"
       @scheduler = WordScheduler.new(60, ["Some", "words"], "b")
       ENV["COLOR_URL"] = "test"
       expect(RestClient).to receive(:post).with(ENV["COLOR_URL"] + '/color', {:word => "Some", :color => "b"})
       expect(RestClient).to receive(:post).with(ENV["COLOR_URL"] + '/color', {:word => "words", :color => "b"})
       @scheduler.start_sending
+    end
+
+    it "returns the sending job's id" do
+      @scheduler = WordScheduler.new(60, ["Some", "words"], "b")
+      ENV["COLOR_URL"] = "test"
+      @scheduler.start_sending
+      expect(@scheduler.job_id).to_not be_nil
     end
 
   end
