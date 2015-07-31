acf65014df01597697711fb3c5e8718145a82004
j-keravica <jelena.keravica@gmail.com>
2015-07-31 16:28:28 +0200

    Added user specs with validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,20 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_many :pages
+  validates :username,
+            :presence => true,
+            :uniqueness => {
+              :case_sensitive => false
+            }
+  validates :email,
+            :presence => true,
+            :uniqueness => {
+              :case_sensitive => false
+            }
+  validates :reset_password_token,
+            :uniqueness => {
+              :case_sensitive => false
+            }
+  
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,44 @@
+require "rails_helper"
+
+describe User do
+
+  it { should have_db_column(:username) }
+  it { should have_db_column(:email) }
+  it { should have_db_column(:encrypted_password) }
+  it { should have_db_column(:reset_password_token) }
+  it { should have_db_column(:reset_password_sent_at) }
+  it { should have_db_column(:remember_created_at) }
+  it { should have_db_column(:sign_in_count) }
+  it { should have_db_column(:current_sign_in_at) }
+  it { should have_db_column(:last_sign_in_at) }
+  it { should have_db_column(:current_sign_in_ip) }
+  it { should have_db_column(:last_sign_in_ip) }
+  it { should have_db_column(:created_at) }
+  it { should have_db_column(:updated_at) }
+
+  it { should have_db_index(:username) }
+  it { should have_db_index(:email) }
+  it { should have_db_index(:reset_password_token) }
+
+  describe "checks uniqueness" do
+    before do
+      User.create!(
+        {
+          :email => "someemail@example.com",
+          :username => "username",
+          :password => "password",
+          :password_confirmation => "password"
+        }
+      )
+    end
+
+    it { should validate_uniqueness_of(:username) }
+    it { should validate_uniqueness_of(:email) }
+    it { should validate_uniqueness_of(:reset_password_token) }
+
+    it { should validate_presence_of(:username) }
+    it { should validate_presence_of(:email) }
+
+  end
+
+end
