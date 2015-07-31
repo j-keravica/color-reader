b1054d0d8a4ef5312ed6cb5d8e7db427adb36b62
j-keravica <jelena.keravica@gmail.com>
2015-07-31 14:54:51 +0200

    Fixed indentation

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,6 +1,6 @@
 class Page < ActiveRecord::Base
 
-	belongs_to :user
+  belongs_to :user
   validates :url, :presence => true
   validates :text, :presence => true
   
--- spec/models/page_spec.rb
+++ spec/models/page_spec.rb
@@ -8,7 +8,7 @@ describe Page do
 
   it { should have_db_index(:user_id) }
   it { should belong_to(:user) }
-  
+
   it { should validate_presence_of(:url) }
   it { should validate_presence_of(:text) }
 
