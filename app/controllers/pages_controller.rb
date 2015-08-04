6e489fa4a6ed820a3f8e7545b291c815f56ea340
j-keravica <jelena.keravica@gmail.com>
2015-08-04 13:50:57 +0200

    Shows correct messages in the view
    Appropriate messages are being displayed for invalid format url and unreachable url.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,7 +8,16 @@ class PagesController < ApplicationController
 
   def create
     url = params[:page][:url]
-    extracted_page = PageExtractor.new(url)
+    begin
+      extracted_page = PageExtractor.extract_page(url)
+    rescue Exception => e
+      @message = e.message
+      respond_to do |format|   
+        format.js { render "invalid" }
+      end
+      return
+    end
+
     current_user.pages.create(:url => url, :title => extracted_page.title, :text => extracted_page.text)
 
     job_id = WordScheduler.start(params[:page][:speed].to_i, extracted_page.text.split, params[:page][:color])
--- features/reading.feature
+++ features/reading.feature
@@ -6,10 +6,15 @@ Feature: Reading
 
   @javascript
   Scenario:
-    When I fill in the form
+    When I fill in the form with a valid url
     Then I should see a message saying the text is being processed
 
   @javascript
   Scenario:
     When I fill in the form with a bad url
     Then I should see a message saying the url is invalid
+
+  @javascript
+  Scenario:
+    When I fill in the form with a url that cannot be reached
+    Then I should see a message saying the url is unreachable
