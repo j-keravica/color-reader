65d500778b7349948609a0231f2399c0105017a8
j-keravica <jelena.keravica@gmail.com>
2016-07-26 16:49:44 +0200

    Add words method for page

--- app/models/page.rb
+++ app/models/page.rb
@@ -2,6 +2,21 @@ class Page < ActiveRecord::Base
 
   belongs_to :user
   validates :url, :presence => true
-  validates :text, :presence => true
-  
+  validates :text
+
+  def words
+    unless text.present?
+      RestClient.post(
+        ENV["EXTRACT_URL"),
+        {
+          :url => url
+        }
+      )
+      response = JSON.parse(response)
+      update_attributes!(:text => response["text"])
+    else
+      text
+    end
+  end
+
 end
