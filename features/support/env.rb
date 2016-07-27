72dc22169cd2236bc9a0f48b9aed8e51798058cb
j-keravica <jelena.keravica@gmail.com>
2016-07-27 15:32:45 +0200

    Change javascript driver to chrome

--- features/support/env.rb
+++ features/support/env.rb
@@ -11,6 +11,14 @@ require 'cucumber/rails'
 # selectors in your step definitions to use the XPath syntax.
 # Capybara.default_selector = :xpath
 
+Capybara.register_driver :chrome do |app|
+  client = Selenium::WebDriver::Remote::Http::Default.new
+  client.timeout = 60
+  Capybara::Selenium::Driver.new(app, :browser => :chrome, :http_client => client)
+end
+
+Capybara.javascript_driver = :chrome
+
 # By default, any exception happening in your Rails application will bubble up
 # to Cucumber so that your scenario will fail. This is a different from how
 # your application behaves in the production environment, where an error page will
